2b3750392cbbd71d0d3ce94576f03923601eadb6
asmit27rai <raiasmit10@gmail.com>
2024-05-16 16:18:40 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/database.js";
+import userRoute from "./routes/userRoute.js";
+import messageRoute from "./routes/messageRoute.js";
+import cookieParser from "cookie-parser";
+
+
+
+
+
+
+
+dotenv.config({});
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cookieParser());
+
+
+app.use("/api/v1/user",userRoute);
+app.use("/api/v1/message",messageRoute);
+
+app.listen(PORT,()=>{
+    connectDB();
+    console.log(`Server listen at ${PORT}`);
+});
